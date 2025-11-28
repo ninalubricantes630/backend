9dd06d5434f88aab003bb0c670b8812cf3a8e3c3
Juancolque04 <jdiazcolque04@gmail.com>
2025-11-28 10:25:49 -0300

    ahora asdk

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -366,15 +366,8 @@ const productosController = {
       updateValues.push(sucursal_id)
 
       if (unidad_medida) {
-        const unidadActual = productos[0].unidad_medida
         updateFields.push("unidad_medida = ?")
         updateValues.push(unidad_medida)
-
-        // Si la unidad de medida cambió, poner el stock en cero
-        if (unidad_medida !== unidadActual) {
-          updateFields.push("stock = ?")
-          updateValues.push(0)
-        }
       }
 
       updateValues.push(id)
@@ -670,4 +663,3 @@ const productosController = {
 }
 
 module.exports = productosController
- 
